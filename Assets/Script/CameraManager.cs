f0b2cb0642e8a2ed5c55ddac56ec6cd8e40832d8
Carkan <rasec.tracip@gmail.com>
2015-12-12 17:08:06 +0100

    Iteration 2
    Déplacement du joueur

---
+++ Assets/Script/CameraManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour {
+
+    float rotationX;
+    float rotationY;
+
+    public GameObject camera;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        rotationX = -Input.GetAxis("R_YAxis_1");
+        rotationY = Input.GetAxis("R_XAxis_1");
+
+        //Debug.Log(rotationX);
+
+        //transform.eulerAngles += new Vector3(rotationX, rotationY, 0f);
+        transform.Rotate((Vector3.right * -Input.GetAxis("R_YAxis_1")) + Vector3.up * Input.GetAxis("R_XAxis_1"));
+        //camera.transform.Rotate((Vector3.right * -Input.GetAxis("R_YAxis_1")) + Vector3.up * Input.GetAxis("R_XAxis_1"));
+	}
+}
---
+++ Assets/Script/MovePlayer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MovePlayer : MonoBehaviour {
+
+    bool aboutToShoot;
+    bool isMoving;
+    float speed;
+    float fadeSpeed;
+    public Image forceGauge;
+
+    public GameObject asteroid;
+
+    Rigidbody rb;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = asteroid.GetComponent<Rigidbody>();
+        speed = 10;
+	}
+    
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.position = asteroid.transform.position;
+        forceGauge.GetComponent<Image>().fillAmount = Input.GetAxis("L_YAxis_1");
+
+        if(Input.GetAxis("L_YAxis_1") > 0)
+        {
+            aboutToShoot = true;
+            speed = Input.GetAxis("L_YAxis_1") * 100;
+        }
+        else if (aboutToShoot)
+        {
+            rb.AddForce(transform.forward * speed);
+            //StartCoroutine("FadeForce");
+            aboutToShoot = false;
+        }
+	}
+
+    IEnumerator FadeForce()
+    {
+        while(fadeSpeed < speed)
+        {
+            rb.AddForce(-transform.forward * fadeSpeed);
+            fadeSpeed += 0.1f;
+            yield return 0;
+        }
+    }
+}
