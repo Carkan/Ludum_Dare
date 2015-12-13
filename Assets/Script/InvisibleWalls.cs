2890f03bf3650552808684e2cc210378c635e5b9
kuarisu <kuarisuran@gmail.com>
2015-12-13 18:27:24 +0100

    Gestion des murs invisibles
    L'animation d'alerte se lance lorsque le joueur sort de la zone jouable.

---
+++ Assets/Script/InvisibleWalls.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvisibleWalls : MonoBehaviour
+{
+
+    bool isOut;
+    public Animator myAnimator;
+
+    // Update is called once per frame
+    void OnTriggerExit(Collider col)
+    {
+        if (col.name == "Comète")
+        {
+            Debug.Log("Sorti !");
+            myAnimator.SetBool("Start_Alert", true);
+            StartCoroutine("PlayerOut");
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.name == "Comète")
+        {
+            myAnimator.SetBool("Start_Alert", false);
+            StopAllCoroutines();
+        }
+    }
+
+
+    IEnumerator PlayerOut()
+    {
+        float time = 0;
+        float timeMax = 10;
+        while (time < timeMax)
+        {
+            time += Time.deltaTime;
+            yield return 0;
+        }
+        myAnimator.SetBool("Start_Alert", false);
+    }
+}
