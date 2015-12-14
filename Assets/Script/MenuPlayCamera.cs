d17aabd791b6adba22b73b60ad303b72e422e36b
kuarisu <kuarisuran@gmail.com>
2015-12-14 01:36:19 +0100

    ité menus
    lol des menus

---
+++ Assets/Script/MenuPlayCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuPlayCamera : MonoBehaviour {
+
+    public Image myImage;
+    public Sprite mySprite;
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 0.1f, 0);
+    
+        if (Input.GetButtonDown("A_0"))
+        {
+            myImage.sprite = mySprite;
+            ButtonStart();
+        }
+    }
+
+
+    public void ButtonStart()
+    {
+        Application.LoadLevel("Main");
+    }
+}
---
+++ Assets/Script/RotationFleche.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RotationFleche : MonoBehaviour {
+
+
+    public Image myImage;
+    public Sprite mySprite;
+
+
+    void Update () {
+        transform.Rotate(0, 0, 2f);
+
+        if (Input.GetButtonDown("A_0"))
+        {
+            myImage.sprite = mySprite;
+            ButtonRestart();
+        }
+    }
+
+    public void ButtonRestart()
+    {
+        Application.LoadLevel("Main");
+    }
+}
